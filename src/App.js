0b917abd902af6b3f6b48f0ff20d9abcb5ceb623
TarqManna <tareqmannan@gmail.com>
2019-04-17 19:32:45 -0400

    Working Counter program yay!

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ class Counter extends Component {
       count: 0,
     }
   }
+/*
+//Here we're going to write custom functionality
+***Increment - is a function that will allow us to increment the Counter
+***Decrement - is a function that will allow us to decrement the Counter
+*/
+
+Increment = () => {
+  // Your code goes here
+  this.setState ({
+    count: this.state.count + 1
+  })
+}
+
+Decrement = () => {
+  //Your code goes here
+  this.setState ({
+    count: this.state.count - 1
+  })
+}
+
 
 
   render () {
@@ -14,9 +34,10 @@ class Counter extends Component {
       <div className="container">
         <div className="navbar">Counter.js</div>
           <div className="counter">
-            <h1>What do we put here?</h1>
-            <button type="button">Increment</button>
-            <button type="button">Decrement</button>
+            <h1>{this.state.count}</h1>
+            <button type="button" onClick = {this.Increment}>Increment</button>
+            <button type="button" onClick = {this.Decrement}>Decrement</button>
+            <button type="button" onClick = {this.Decrement}>Decrement</button>
         </div>
       </div>
     )
